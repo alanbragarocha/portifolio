6c46eb2c0cec75e985b71b7682ebf676c91fe6b5
alanbragarocha <alanbragarocha@hotmail.com>
2025-02-21 10:29:15 -0300

    logo

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,7 +4,15 @@ var menuBtn = document.querySelector('.menu-btn i');
 function menuShow() {
     if (ul.classList.contains('open')) {
         ul.classList.remove('open');
-    }else{
+    } else {
         ul.classList.add('open');
     }
 }
+
+// Fecha o menu quando um item é clicado
+var menuLinks = document.querySelectorAll('nav ul li a');
+menuLinks.forEach(function(link) {
+    link.addEventListener('click', function() {
+        ul.classList.remove('open');
+    });
+});
